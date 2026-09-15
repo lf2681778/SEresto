24c4fc1641fde4465765fbbfc4473f3dc3adc904
lf2681778 <33818706+lf2681778@users.noreply.github.com>
2026-09-15 17:11:21 +0800

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const XLSX = require('xlsx');
 const USERNAME = process.env.USERNAME;
 const PASSWORD = process.env.PASSWORD;
 // 🌟 這裡使用 CHAIN_CODE 讀取 GitHub Secret，但存進名稱為 chainCode 的變數中，完美對齊 API 文件
-const chainCode = process.env.chainCod; 
+const chainCode = process.env.chainCode; 
 const PLATFORM_KEY = process.env.PLATFORM_KEY; 
 
 const ERROR_THRESHOLD = 100; 
